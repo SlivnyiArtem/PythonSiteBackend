31185d7cfd0331f899aef25ab8140c15d388a964
SlivnyiArtem <slivnay1965@gmail.com>
2023-02-15 15:42:39 +0500

    complite migration

--- src/app/internal/bot.py
+++ src/app/internal/bot.py
@@ -11,16 +11,14 @@ from app.internal.models.simple_user import SimpleUser
 class Bot:
     def __init__(self):
         self.application = telebot.TeleBot('6007627790:AAG3MqSDVIFkzfCRmYNDJNWc1UOqFpHxYdw')
-
-        @self.application.message_handler(commands=['start'])
-        def send_welcome(message):
-            SimpleUser.objects.update_or_create(user_id = message.from_user.id, name = "message.from_user.name", surname = "message.from_user.name")
-            # SimpleUser.objects.update_or_create(user_id=messagef.effective_user.id, name=update.effective_user.first_name,
-            #                                     surname=update.effective_user.last_name)
-            self.application.reply_to(message, """\
-        Hi there, I am EchoBot.
-        I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-        """)
+        self.application.message_handler(commands=['start'])(lambda message:
+                                                             handlers.start_handler(message, self.application))
+        # @self.application.message_handler(commands=['start'])
+        # lambda message: handlers.start_handler(message, self.application)
+        '''
+        def start(message):
+            handlers.start_handler(message, self.application)
+        '''
 
         # self.application = ApplicationBuilder().token('6007627790:AAG3MqSDVIFkzfCRmYNDJNWc1UOqFpHxYdw').build()
         # start_handler = CommandHandler('start', handlers.start_handler)
--- src/app/internal/transport/bot/handlers.py
+++ src/app/internal/transport/bot/handlers.py
@@ -7,12 +7,25 @@ from telegram.ext import ContextTypes
 from app.internal.models.simple_user import SimpleUser
 
 
-async def start_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print(update.effective_user.name)
-    print("@@Start")
-    create_user(update)
+def start_handler(message, bot):
+    user = message.from_user
+    print(user.first_name)
+    default_updates = {"name": user.first_name, "surname": user.last_name}
+    SimpleUser.objects.update_or_create(user_id=user.id, defaults=default_updates)
+    # SimpleUser.objects.update_or_create(user_id=messagef.effective_user.id, name=update.effective_user.first_name,
+    #                                     surname=update.effective_user.last_name)
+#     bot.reply_to(message, """\
+# Hi there, I am EchoBot.
+# I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+# """)
 
-
-def create_user(update: Update):
-    print(update.effective_user.id)
-    sync_to_async(SimpleUser.objects.update_or_create(user_id=update.effective_user.id, name=update.effective_user.first_name, surname=update.effective_user.last_name, phone_number = None))
+#
+# async def start_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+#     print(update.effective_user.name)
+#     print("@@Start")
+#     create_user(update)
+#
+#
+# def create_user(update: Update):
+#     print(update.effective_user.id)
+#     sync_to_async(SimpleUser.objects.update_or_create(user_id=update.effective_user.id, name=update.effective_user.first_name, surname=update.effective_user.last_name, phone_number = None))
