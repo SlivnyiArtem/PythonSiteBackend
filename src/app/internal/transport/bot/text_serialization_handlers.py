448dbfbdc042d48ca31403517d42e5d7f070e64d
SlivnyiArtem <slivnay1965@gmail.com>
2023-03-14 17:08:46 +0500

    serialization imprv

--- src/app/internal/transport/bot/text_serialization_handlers.py
+++ src/app/internal/transport/bot/text_serialization_handlers.py
@@ -1,4 +1,5 @@
 def convert_dict_to_str(input_dict: dict) -> str:
+    input_dict.pop("error_code")
     result_str = ""
     for key, value in input_dict.items():
         result_str += f"{str(key)} : {str(value)}\n"
