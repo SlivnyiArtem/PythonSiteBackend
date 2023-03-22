b13530220c8821770fcb6710272b67ff4ff4e093
SlivnyiArtem <slivnay1965@gmail.com>
2023-03-23 03:06:03 +0500

    solve problem with differences on local/docker

--- src/config/settings_local.py
+++ src/config/settings_local.py
@@ -85,7 +85,7 @@ DATABASES = {
         "NAME": env("DB_NAME"),
         "USER": env("DB_USER"),
         "PASSWORD": env("DB_PASSWORD"),
-        "HOST": env("DB_HOST"),
+        "HOST": "host.docker.internal",
     }
 }
 
