49f9987ea6004b3ca1f8447d27e9a554cb7d96d5
SlivnyiArtem <slivnay1965@gmail.com>
2023-04-05 01:42:51 +0500

    one more attempt

--- src/app/internal/bot.py
+++ src/app/internal/bot.py
@@ -1,10 +1,10 @@
 import environ
-import telebot
+# import telebot
 
 from app.internal.transport.bot import handlers
 
-# import telegram
-# from telegram.ext import CommandHandler, Updater
+import telegram
+from telegram.ext import CommandHandler, Updater
 
 
 # from telegram import Bot as b
@@ -14,63 +14,59 @@ env = environ.Env()
 environ.Env.read_env()
 
 
-class Bot:
-    def __init__(self):
-        self.application = telebot.TeleBot(env("BOT_KEY"))
-        self.application.message_handler(commands=["help"])(
-            lambda message: handlers.help_handler(message, self.application)
-        )
-        self.application.message_handler(commands=["start"])(
-            lambda message: handlers.start_handler(message, self.application)
-        )
-
-        self.application.message_handler(commands=["set_phone"])(
-            lambda message: handlers.phone_number_handler(message, self.application)
-        )
-
-        self.application.message_handler(commands=["me"])(
-            lambda message: handlers.me_inf_handler(message, self.application)
-        )
-
-        self.application.message_handler(commands=["check_currency"])(
-            lambda message: handlers.currency_amount_handler(message, self.application)
-        )
-
-    def start(self):
-        self.application.infinity_polling()
-
-
-def start_bot():
-    bot = Bot()
-    bot.start()
-
-
 # class Bot:
 #     def __init__(self):
-#         self.application = Application.builder().token(env("BOT_KEY")).build()
-#         self.application.add_handler(CommandHandler("help", handlers.help_handler))
-
-
-# class Bot:
-#     def __init__(self):
-#         self.application = telegram.Bot(env("BOT_KEY"))
-#         self.updater = Updater(env("BOT_KEY"))
-#         self.dispatcher = self.updater.dispatcher
-#         self.dispatcher.add_handler(CommandHandler("help", handlers.help_handler))
+#         self.application = telebot.TeleBot(env("BOT_KEY"))
+#         self.application.message_handler(commands=["help"])(
+#             lambda message: handlers.help_handler(message, self.application)
+#         )
+#         self.application.message_handler(commands=["start"])(
+#             lambda message: handlers.start_handler(message, self.application)
+#         )
+#
+#         self.application.message_handler(commands=["set_phone"])(
+#             lambda message: handlers.phone_number_handler(message, self.application)
+#         )
+#
+#         self.application.message_handler(commands=["me"])(
+#             lambda message: handlers.me_inf_handler(message, self.application)
+#         )
+#
+#         self.application.message_handler(commands=["check_currency"])(
+#             lambda message: handlers.currency_amount_handler(message, self.application)
+#         )
+#
+#     def start(self):
+#         self.application.infinity_polling()
 #
 #
 # def start_bot():
 #     bot = Bot()
-#     bot.updater.start_webhook(
-#         listen="127.0.0.1",
-#         port=5000,
-#         url_path=env("BOT_KEY"),
-#         key="/etc/letsencrypt/live/${MY_DOMEN}/privkey.pem",
-#         cert="/etc/letsencrypt/live/${MY_DOMEN}/fullchain.pem",
-#         webhook_url="${MY_DOMEN}/${BOT_KEY}",
-#     )
-#     bot.updater.idle()
-#     # bot.updater.start_polling()
+#     bot.start()
+
+
+
+
+class Bot:
+    def __init__(self):
+        self.application = telegram.Bot(env("BOT_KEY"))
+        self.updater = Updater(env("BOT_KEY"))
+        self.dispatcher = self.updater.dispatcher
+        self.dispatcher.add_handler(CommandHandler("help", handlers.help_handler))
+
+
+def start_bot():
+    bot = Bot()
+    bot.updater.start_webhook(
+        listen="127.0.0.1",
+        port=5000,
+        url_path=env("BOT_KEY"),
+        key="/etc/letsencrypt/live/${MY_DOMEN}/privkey.pem",
+        cert="/etc/letsencrypt/live/${MY_DOMEN}/fullchain.pem",
+        webhook_url="${MY_DOMEN}/${BOT_KEY}",
+    )
+    bot.updater.idle()
+    # bot.updater.start_polling()
 
 
 if __name__ == "__main__":
