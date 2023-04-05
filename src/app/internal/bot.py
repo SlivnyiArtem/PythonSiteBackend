f5cc789b9115db06151929df1be6629c4bc3e928
SlivnyiArtem <slivnay1965@gmail.com>
2023-04-06 00:29:14 +0500

    cleaning+lintering

--- src/app/internal/bot.py
+++ src/app/internal/bot.py
@@ -56,65 +56,6 @@ class Bot:
         self.application.infinity_polling()
 
 
-# class Updater(APIView):
-#     def post(self, request):
-#         json_str = request.body.decode("UTF-8")
-#         print(json_str)
-#         update = types.Update.de_json(json_str)
-#         bot.application.process_new_updates([update])
-#
-#         return Response({"code": 200})
-
-
-# class Bot:
-#     def __init__(self):
-#         self.application = telegram.Bot(env("BOT_KEY"))
-#         self.updater = Updater(env("BOT_KEY"))
-#         self.dispatcher = self.updater.dispatcher
-#         self.dispatcher.add_handler(CommandHandler("help", handlers.help_handler))
-#
-#
-# def gg():
-#     with open("privkey.pem") as f:
-#         f.read()
-#
-#
-# def start_bot():
-#     # gg()
-#     # raise Exception("https://"+env("MY_DOMEN")+"/"+env("BOT_KEY"))
-#     bot = Bot()
-#     bot.updater.start_webhook(
-#         # listen="158.160.52.96",
-#         # port=443,
-#         url_path=env("BOT_KEY"),
-#         webhook_url="https://" + env("MY_DOMEN") + "/" + env("BOT_KEY"),
-#     )
-#     # bot.updater.start_webhook(
-#     #     # listen="127.0.0.1",
-#     #     # port=5000,
-#     #     url_path=env("BOT_KEY"),
-#     #     # cert=get_key(),
-#     #     # key="/etc/letsencrypt/live/${MY_DOMEN}/privkey.pem",
-#     #     # cert="/etc/letsencrypt/live/${MY_DOMEN}/fullchain.pem",
-#     #     cert ="pythonbackend2023/privkey.pem",
-#     #
-#     #     webhook_url="https://"+env("MY_DOMEN")+"/"+env("BOT_KEY"),
-#     # )
-#     bot.updater.idle()
-#     # bot.updater.start_polling()
-
-
-# def initial_start():
-# bot = Bot()
-# bot.start()
-
-
 if __name__ == "__main__":
     bot = Bot()
     bot.start()
-    # start_bot(bot)
-    # bot = Bot()
-    # bot.start()
-    # # start_bot(bot)
-    # bot = Bot()
-    # initial_start()
