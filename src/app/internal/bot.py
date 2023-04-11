a0ca1267b4d9fd5c8128b252fdce661ef75f24c1
SlivnyiArtem <slivnay1965@gmail.com>
2023-04-11 09:22:04 +0500

    lint

--- src/app/internal/bot.py
+++ src/app/internal/bot.py
@@ -48,8 +48,9 @@ class Bot:
     def start(self):
         self.application.remove_webhook()
         self.application.run_webhooks(
-            listen="0.0.0.0", port=5000, webhook_url="https://" + env("MY_DOMEN") + "/bot/" +
-                                                     env("BOT_KEY_1") + ":" + env("BOT_KEY_2")
+            listen="0.0.0.0",
+            port=5000,
+            webhook_url="https://" + env("MY_DOMEN") + "/bot/" + env("BOT_KEY_1") + ":" + env("BOT_KEY_2"),
         )
 
 
