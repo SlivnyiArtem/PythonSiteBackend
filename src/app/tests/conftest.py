7e5026a903dff466c12014e61feda70c529ac567
SlivnyiArtem <slivnay1965@gmail.com>
2023-04-12 17:34:08 +0500

    more tests

--- src/app/tests/conftest.py
+++ src/app/tests/conftest.py
@@ -10,11 +10,15 @@ from app.internal.models.simple_user import SimpleUser
 def test_card_number():
     return 7777
 
-
 @pytest.fixture(scope="function")
-def test_banking_account() -> BankingAccount:
-    acc = BankingAccount.objects.create(account_number=7, account_owner=789, currency_amount=500)
-    return acc
+def test_acc_number():
+    return 1
+
+
+# @pytest.fixture(scope="function")
+# def test_banking_account() -> BankingAccount:
+#     acc = BankingAccount.objects.create(account_number=7, account_owner=789, currency_amount=500)
+#     return acc
 
 
 @pytest.fixture(scope="function")
@@ -22,8 +26,8 @@ def test_card() -> Card:
     card = Card.objects.create(card_number=7777, MM=5, YY=2020, system="VISA", banking_account=get_test_bank_acc())
     return card
 
-
-def get_test_bank_acc():
+@pytest.fixture(scope="function")
+def test_bank_acc():
     simple_user_1 = SimpleUser.objects.create(user_id=123, full_username="tzinch")
     acc = BankingAccount.objects.create(account_number=1, account_owner=simple_user_1, currency_amount=500)
     return acc
--- src/app/tests/test_services.py
+++ src/app/tests/test_services.py
@@ -1,6 +1,7 @@
 import pytest
 
-from app.internal.services.banking_service import get_card_by_id
+from app.internal.services.banking_service \
+    import get_card_by_id, get_acc_by_user, get_acc_by_id
 
 # Создать тестового юзера и тестоую карту
 
@@ -14,10 +15,17 @@ from app.internal.services.banking_service import get_card_by_id
 # получение юзера по ид
 
 
-# ТРАНЗАКЦИИ
+# ТРАНЗАКЦИИ(3 типа)
 
 
 @pytest.mark.django_db
 def test_get_card_by_number_ok(test_card_number, test_card):
     card = get_card_by_id(test_card_number)
     assert card == test_card
+
+@pytest.mark.django_db
+def test_get_acc_by_acc_number_ok(test_acc_number, test_bank_acc):
+    acc = get_acc_by_id(test_acc_number)
+    assert acc == test_bank_acc
+
+
