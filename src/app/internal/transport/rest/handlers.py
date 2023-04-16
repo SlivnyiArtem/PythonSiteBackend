dfeaf6250d485a6dc9b511caf368e0870f10ec0a
SlivnyiArtem <slivnay1965@gmail.com>
2023-04-16 20:09:09 +0500

    add testpage handler

--- src/app/internal/transport/rest/handlers.py
+++ src/app/internal/transport/rest/handlers.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 
 from app.internal.transport.information_former import form_information_handlers
 
@@ -9,4 +9,4 @@ def me_http_inf_handler(_, user_id: int):
 
 
 def test_page():
-    return JsonResponse({"msg": "connection established"}, json_dumps_params={"ensure_ascii": False})
+    return HttpResponse("<h1>connection established</h1>")
--- src/config/urls.py
+++ src/config/urls.py
@@ -5,8 +5,10 @@ from django.urls import include, path
 from django.views.decorators.csrf import csrf_exempt
 
 from app.internal.bot import Bot
+from app.internal.transport.rest import handlers
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("app.internal.urls")),
+    path("testpage/", handlers.test_page),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
