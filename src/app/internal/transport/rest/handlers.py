21baf08dbb59da6df0ce1a6e5ee3bf6c9f7e4c8e
SlivnyiArtem <slivnay1965@gmail.com>
2023-04-16 19:56:30 +0500

    add testpage

--- src/app/internal/transport/rest/handlers.py
+++ src/app/internal/transport/rest/handlers.py
@@ -6,3 +6,7 @@ from app.internal.transport.information_former import form_information_handlers
 def me_http_inf_handler(_, user_id: int):
     information = form_information_handlers.get_user_information(user_id)
     return JsonResponse(information, json_dumps_params={"ensure_ascii": False}, status=information["error_code"])
+
+
+def test_page():
+    return JsonResponse({"msg": "connection established"}, json_dumps_params={"ensure_ascii": False})
--- src/app/internal/urls.py
+++ src/app/internal/urls.py
@@ -5,4 +5,5 @@ from app.internal.transport.rest import handlers
 
 urlpatterns = [
     path("users/<int:user_id>/me", handlers.me_http_inf_handler),
+    path("testpage", handlers.test_page),
 ]
