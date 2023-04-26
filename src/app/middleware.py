dff4feea69bd8234792ca25f7cbdffac64764c8c
SlivnyiArtem <slivnay1965@gmail.com>
2023-04-27 03:50:38 +0500

    .

--- src/app/middleware.py
+++ src/app/middleware.py
@@ -1,8 +1,8 @@
 def auth_middleware(get_response):
     def auth(request):
-        raise Exception
-        response = get_response(request)
+        # raise Exception
         print("Check")
+        response = get_response(request)
         return response
 
     return auth
