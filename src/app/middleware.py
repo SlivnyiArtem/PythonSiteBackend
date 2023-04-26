029ad09826b17826d0675a0e2e98a09ce7ddd65f
SlivnyiArtem <slivnay1965@gmail.com>
2023-04-27 03:58:20 +0500

    comment test

--- src/app/middleware.py
+++ src/app/middleware.py
@@ -1,6 +1,6 @@
 def auth_middleware(get_response):
     def auth(request):
-        # raise Exception
+        raise Exception
         print("Check")
         response = get_response(request)
         return response
