60bb0b2383312d2e6a16d64341ea8838c1e2b9dd
SlivnyiArtem <slivnay1965@gmail.com>
2023-04-27 08:31:52 +0500

    .

--- src/app/admin.py
+++ src/app/admin.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from app.internal.models.banking_account import BankingAccount
 from app.internal.models.banking_card import Card
 from app.internal.models.simple_user import SimpleUser
+from internal.models.token import RefreshToken
 
 admin.site.site_title = "Backend course"
 admin.site.site_header = "Backend course"
 admin.site.register(SimpleUser)
 admin.site.register(BankingAccount)
 admin.site.register(Card)
+admin.site.register(RefreshToken)
