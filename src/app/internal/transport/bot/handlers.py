2d68834b67b3c1991e8c2b3375e95d845d8c8021
SlivnyiArtem <slivnay1965@gmail.com>
2023-04-27 19:02:22 +0500

    fixing problems

--- src/app/internal/transport/bot/handlers.py
+++ src/app/internal/transport/bot/handlers.py
@@ -363,8 +363,8 @@ def login_handler(message: telebot.types.Message, bot):
         if len(list(access_token)) == 0 and len(list(refresh_token)) == 0:
             msg = bot.send_message(message.chat.id, "Введите свой пароль")
             bot.register_next_step_handler(msg, check_password, bot)
-        elif token_service.check_is_expired(access_token):
-            if refresh_token is None or token_service.check_is_expired(refresh_token):
+        elif token_service.check_is_expired(access_token.first()):
+            if refresh_token is None or token_service.check_is_expired(refresh_token.first()):
                 # отзываем оба токена V
                 token_service.revoke_all_tokens_for_user(user)
 
@@ -382,14 +382,10 @@ def login_handler(message: telebot.types.Message, bot):
 
 def check_password(message: telebot.types.Message, bot):
     user = user_service.get_user_by_id(message.from_user.id)
-    bot.send_message(message.chat.id, "pog")
     if len(message.text) > 0 and password_service.get_hash_from_password(message.text) == user.hash_of_password:
-        bot.send_message(message.chat.id, "pog2")
         token_service.update_and_get_tokens(user)
-        bot.send_message(message.chat.id, "pog3")
         add_rights(user)
     else:
-        bot.send_message(message.chat.id, "pog4")
         bot.send_message(message.chat.id, "ваш пароль не верный")
 
 
