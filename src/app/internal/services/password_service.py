abccf68026e67d19949df1edec59d5a3377e97d9
SlivnyiArtem <slivnay1965@gmail.com>
2023-04-27 19:16:40 +0500

    more fixes + cleaning

--- src/app/internal/services/password_service.py
+++ src/app/internal/services/password_service.py
@@ -3,9 +3,6 @@ import uuid
 
 
 def get_hash_from_password(password: str) -> str:
-    # hash_salt =
-    # hash_salt = ""
-    result = hashlib.sha512(password.encode("utf-8")).hexdigest()
-    # result = hashlib.sha512(password.encode('utf-8'))
-    # raise Exception(result)
+    hash_salt = uuid.uuid4().hex
+    result = hashlib.sha512(password.encode("utf-8") + hash_salt.encode("utf-8")).hexdigest()
     return result
