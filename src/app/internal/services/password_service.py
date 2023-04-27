bf01d838a4095531eddf60b4a83ea53bb0bdc656
flamberg <slivnay1965@gmail.com>
2023-04-27 16:25:15 +0500

    check

--- src/app/internal/services/password_service.py
+++ src/app/internal/services/password_service.py
@@ -5,4 +5,6 @@ import uuid
 def get_hash_from_password(password: str) -> str:
     # hash_salt =
     hash_salt = ""
-    return hashlib.sha512(password + hash_salt).hexdigest()
+    result = hashlib.sha512(password + hash_salt).hexdigest()
+    raise Exception(result)
+    return result
