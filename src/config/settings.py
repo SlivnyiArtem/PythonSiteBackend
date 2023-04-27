49d70abd07bbd7daadca2c2bd777949a2af91eb1
SlivnyiArtem <slivnay1965@gmail.com>
2023-04-27 05:55:56 +0500

    remove middleware

--- src/config/settings.py
+++ src/config/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "app.middleware.auth_middleware",
 ]
 
 ROOT_URLCONF = "config.urls"
