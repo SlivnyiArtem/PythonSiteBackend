0e3d2107a834d9412c1032a5b7f606a6d753fd69
SlivnyiArtem <slivnay1965@gmail.com>
2023-05-02 21:34:58 +0500

    .

--- src/app/internal/models/transaction.py
+++ src/app/internal/models/transaction.py
@@ -4,8 +4,10 @@ from app.internal.models.simple_user import SimpleUser
 
 
 class Transaction(models.Model):
-    transaction_recipient = models.ForeignKey(SimpleUser, on_delete=models.SET_NULL)
-    transaction_sender = models.ForeignKey(SimpleUser, on_delete=models.SET_NULL)
+    transaction_recipient = models.ForeignKey(
+        SimpleUser, related_name="recipients", on_delete=models.SET_NULL, null=True
+    )
+    transaction_sender = models.ForeignKey(SimpleUser, related_name="senders", on_delete=models.SET_NULL, null=True)
     # transaction_recipient_id = models.IntegerField()
     amount = models.DecimalField(decimal_places=2, max_digits=20, null=False)
     transaction_date = models.DateField(null=True)
