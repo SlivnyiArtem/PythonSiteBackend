0d8ed08a0a487260056c5cf07b74346ac6a525b7
SlivnyiArtem <slivnay1965@gmail.com>
2023-05-03 03:51:11 +0500

    .

--- src/app/middlewares/middleware.py
+++ src/app/middlewares/middleware.py
@@ -1,7 +1,7 @@
 def auth_middleware(get_response):
     def auth(request):
         response = get_response(request)
-        response["mabooka"] = "mother"
+        response["mabooka"] = "great_mother"
         # return response
 
     return auth
