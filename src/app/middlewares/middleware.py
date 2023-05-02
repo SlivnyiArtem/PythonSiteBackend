747a6f459a2103de106f68442d828e758ab4bd16
SlivnyiArtem <slivnay1965@gmail.com>
2023-05-03 04:28:01 +0500

    Revert "."
    This reverts commit 4e657662cb9a567890dbdfb1a8de79693f594e05.

--- src/app/middlewares/middleware.py
+++ src/app/middlewares/middleware.py
@@ -1,19 +1,8 @@
-import json
-
-
 def auth_middleware(get_response):
     def auth(request):
-        # request_2 = request.GET.copy()
-
-        request_data = getattr(request, "_body", request.body)
-        request_data = json.loads(request_data)
-
-        request_data["mabooka"] = "great_mother"
-        # here you can write the logic to append the payload to request data
-        request._body = json.dumps(request_data)
-
-        # request_2["mabooka"] = "great_mother"
-        # request.GET = request_2
+        request_2 = request.GET.copy()
+        request_2["mabooka"] = "great_mother"
+        request.GET = request_2
         response = get_response(request)
 
         return response
