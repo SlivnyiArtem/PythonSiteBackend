b70ce306b847509e7c3a31e7314cb3634ab23c2f
SlivnyiArtem <slivnay1965@gmail.com>
2023-05-02 21:12:31 +0500

    lint

--- src/app/internal/models/transaction_log.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class TransactionLog(models.Model):
-    transaction_recipient_id = models.IntegerField()
-    amount = models.DecimalField(decimal_places=2, max_digits=20, null=False)
-    transaction_date = models.DateField(null=True)
-    is_outgoing_transaction = models.BooleanField(null=False)
