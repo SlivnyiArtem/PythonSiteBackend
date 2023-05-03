ee642764c9fb9414b68574a70fcdd3af6112cd7f
SlivnyiArtem <slivnay1965@gmail.com>
2023-05-04 02:07:08 +0500

    .

--- src/app/middlewares/middleware.py
+++ src/app/middlewares/middleware.py
@@ -11,14 +11,59 @@ env = environ.Env()
 environ.Env.read_env()
 
 
+def authentificate(request: HttpRequest):
+    if "userapi" not in request.path:
+        return None
+        # return get_response(request)
+        # return JsonResponse({"Ok": "OK"})
+
+    else:
+        raw_acc_token = request.headers.get("acc_token")
+        user = user_service.get_user_by_id(request.headers.get("user_id"))
+        refresh_token_obj = RefreshToken.objects.filter(user=user).first()
+
+        if raw_acc_token is None or token_service.check_is_token_expired(
+            token_service.get_token_data(raw_acc_token), env("EXPIRE_TIME_ACCESS")
+        ):
+            if refresh_token_obj is None:
+                return HttpResponse(
+                    json.dumps({"login": "not_ok", "user_id": user.user_id}),
+                    content_type="application/json",
+                )
+                # return JsonResponse({"Login": "Логинься"})
+                pass
+            else:
+                raw_refresh_token = refresh_token_obj.Jti
+                refresh_token_data = token_service.get_token_data(raw_refresh_token)
+                if token_service.check_is_token_expired(refresh_token_data, env("EXPIRE_TIME_REFRESH")):
+                    token_service.revoke_all_tokens_for_user(user)
+                    return test_page(None)
+                else:
+                    raw_refresh_token, raw_acc_token = token_service.update_and_get_tokens(user, refresh_token_obj)
+                    return HttpResponse(
+                        json.dumps(
+                            token_service.create_json_response_for_tokens(raw_refresh_token, raw_acc_token, user)
+                        ),
+                        content_type="application/json",
+                    )
+        else:
+            return None
+    #                 # return JsonResponse({"Ok": "OK"})
+    #                 return get_response(request)
+
+
 class AuthMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        response = self.get_response(request)
-        response["Test"] = "Test"
-        return JsonResponse({"AS": response["user_id"]})
+        # response = self.get_response(request)
+        # response["Test"] = "Test"
+        response = authentificate(request)
+        if response is None:
+            return self.get_response(request)
+        return response
+        # return JsonResponse({"AS": response["user_id"]})
 
 
 # def auth_middleware(get_response):
