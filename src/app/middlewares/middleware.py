b9512b40c0d9598b64b9bef8492fd57c1bea0da7
flamberg <slivnay1965@gmail.com>
2023-05-03 09:15:05 +0500

    test

--- src/app/middlewares/middleware.py
+++ src/app/middlewares/middleware.py
@@ -1,10 +1,13 @@
+from django.http import JsonResponse
+
+
 def auth_middleware(get_response):
     def auth(request):
         request_2 = request.GET.copy()
         request_2["mabooka"] = "great_mother"
         request.GET = request_2
-        response = get_response(request)
+        # response = get_response(request)
 
-        return response
+        return JsonResponse({"err": "err"}, status=404)
 
     return auth
