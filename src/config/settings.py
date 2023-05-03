1f3350e5a2cb04d08e9cd9937e2ad7bc7375d9b2
SlivnyiArtem <slivnay1965@gmail.com>
2023-05-04 01:43:57 +0500

    .

--- src/config/settings.py
+++ src/config/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "app.middlewares.middleware.auth_middleware",
+    "app.middlewares.middleware.AuthMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
