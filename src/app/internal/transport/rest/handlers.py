a837b396e5a23771049c0dd5fd557afcab66dce7
SlivnyiArtem <slivnay1965@gmail.com>
2023-05-04 01:15:05 +0500

    change json to http

--- src/app/internal/transport/rest/handlers.py
+++ src/app/internal/transport/rest/handlers.py
@@ -22,7 +22,12 @@ def headers(headers_dict):
 
 def me_http_inf_handler(_, user_id: int):
     information = form_information_handlers.get_user_information(user_id)
-    response = JsonResponse(information, json_dumps_params={"ensure_ascii": False}, status=information["error_code"])
+    response = HttpResponse(
+        json.dumps(information),
+        content_type="application/json",
+        status=information["error_code"]
+    )
+    # response = JsonResponse(information, json_dumps_params={"ensure_ascii": False}, status=information["error_code"])
     response["user_id"] = user_id
     return response
 
