2130263854cfc558ed3ce285036fe689be35c38b
flamberg <slivnay1965@gmail.com>
2023-05-03 10:09:56 +0500

    add user search

--- src/app/middlewares/middleware.py
+++ src/app/middlewares/middleware.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 
-from app.internal.services import token_service
+from app.internal.services import token_service, user_service
 
 
 def auth_middleware(get_response):
@@ -10,7 +10,11 @@ def auth_middleware(get_response):
         # request.GET = request_2
         # content = {"err": "err"}
 
-        json_tokens_response = token_service.update_and_get_tokens()
+        response = get_response(request)
+
+        user = user_service.get_user_by_id(response.context["user_id"])
+
+        json_tokens_response = token_service.update_and_get_tokens(user)
         return json_tokens_response
         # return JsonResponse(tokens_response, status=404)
 
