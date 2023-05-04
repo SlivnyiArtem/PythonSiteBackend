bcf75ec4a182682d97b6f88328978b85364e850c
flamberg <slivnay1965@gmail.com>
2023-05-04 14:17:11 +0500

    test wrapper

--- src/config/urls.py
+++ src/config/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from app.internal.transport.rest import handlers, me, user_login
+from app.internal.transport.rest import handlers, me_endpoint, user_login_endpoint
 
 # from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -12,8 +12,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("app.internal.urls")),
     path("userapi/<int:user_id>/newtestpage/", handlers.test_page_new),
-    path("login/", user_login.UserLoginView.as_view()),
-    path("userapi/<int:user_id>/me", me.MeInfView.as_view()),
+    path("login/", user_login_endpoint.UserLoginView.as_view()),
+    path("userapi/<int:user_id>/me", me_endpoint.MeInfView.as_view()),
     # path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     # path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
