8d0a34504b284d20fe7662966ea9fcb4b944f66b
SlivnyiArtem <slivnay1965@gmail.com>
2023-05-04 09:32:52 +0500

    one more argument

--- src/app/internal/transport/rest/user_login.py
+++ src/app/internal/transport/rest/user_login.py
@@ -9,7 +9,7 @@ from app.internal.services import password_service, token_service, user_service
 
 @csrf_exempt
 class UserLoginView(View):
-    def post(self, response: HttpResponse):
+    def post(self, _, response: HttpResponse):
         # json_data = json.loads(request.body)
         user = user_service.get_user_by_id(response.headers.get("user_id"))
         # user_login = json_data["login"]
