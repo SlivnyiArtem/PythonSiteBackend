1e69814dd7938dbcd174e01b7784dbb17362a994
SlivnyiArtem <slivnay1965@gmail.com>
2023-05-09 15:14:40 +0500

    fix serializer

--- src/app/internal/transport/rest/serializer.py
+++ src/app/internal/transport/rest/serializer.py
@@ -1,8 +1,16 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+from app.internal.models.simple_user import SimpleUser
+
+
+class UserAuthSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ["user_id", "password"]
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = get_user_model()
+        model = SimpleUser
         fields = ["simple_user_id", "full_username", "user_name", "surname"]
