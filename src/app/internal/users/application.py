278c512617b60043c08f911a6493be485c4a1f50
SlivnyiArtem <slivnay1965@gmail.com>
2023-05-18 11:44:49 +0500

    change router initialization

--- src/app/internal/users/application.py
+++ src/app/internal/users/application.py
@@ -4,7 +4,7 @@ from app.internal.transport.rest.Controllers import CustomController
 from app.internal.users.db_data.repositories import UserRepository
 from app.internal.users.domain.services import UserService
 from app.internal.users.presentation.handlers import UserHandlers
-from app.internal.users.presentation.routers import get_users_router
+from app.internal.users.presentation.routers import get_users_router, router
 
 
 def get_api():
@@ -13,7 +13,8 @@ def get_api():
     user_service = UserService(user_repo=user_repo)
     # user_handlers = UserHandlers(user_service=user_service)
     api.register_controllers(CustomController)
-    api.add_router("/users", get_users_router(UserHandlers(user_service=user_service)))  # !!!
+    api.add_router(router)
+    # api.add_router("/users", get_users_router(UserHandlers(user_service=user_service)))  # !!!
 
     # add_users_router(api, user_handlers)
 
--- src/app/internal/users/presentation/routers.py
+++ src/app/internal/users/presentation/routers.py
@@ -1,17 +1,18 @@
+from django.http import HttpResponse, JsonResponse
 from ninja import Router
 from ninja_jwt.authentication import JWTAuth
 
 from app.internal.users.presentation.handlers import UserHandlers
 
+router = Router()
+
 
 def get_users_router(user_handlers: UserHandlers):
-    router = Router()
-
     # router.add_api_operation(
     #     path="/me", auth=JWTAuth(), methods=["GET"], response={}, view_func=user_handlers.me_information
     # )
     #
-    router.add_api_operation(path="/test", methods=["GET"], response={}, view_func=user_handlers.test_information)
+    # router.add_api_operation(path="/test", methods=["GET"], response={}, view_func=user_handlers.test_information)
 
     return router
 
@@ -19,12 +20,13 @@ def get_users_router(user_handlers: UserHandlers):
 # rest_app_router = Router()
 #
 #
-# @rest_app_router.get("/me", auth=JWTAuth())
-# def me_handler(request):
-#     information = form_information_handlers.get_user_information(request.user.username)
-#     return JsonResponse(information, json_dumps_params={"ensure_ascii": False}, status=information["error_code"])
-#
-#
-# @rest_app_router.get("/test")
-# def test_handler(request):
-#     return HttpResponse("TESTTESTTEST")
+@router.get("/me", auth=JWTAuth())
+def me_handler(request):
+    # information = form_information_handlers.get_user_information(request.user.username)
+    information = {"2": "@"}
+    return JsonResponse(information, json_dumps_params={"ensure_ascii": False}, status=information["error_code"])
+
+
+@router.get("/test")
+def test_handler(request):
+    return HttpResponse("TESTTESTTEST")
